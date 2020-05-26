e40862a716fb630351f7137ad198f55e4d04ba93
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-05-26 16:43:51 +1000

    UI list/tag/export list/ create task

--- src/sidebarComponent/AddListName.jsx
+++ src/sidebarComponent/AddListName.jsx
@@ -6,21 +6,21 @@ import Button from "react-bootstrap/Button";
 import { useState } from 'react';
 
 
-const [newListName, setListName] = useState("");
-function onChangeListName(e){
-    var newListName = e.target.value;
-    setListName(newListName);
+// const [newListName, setListName] = useState("");
+// function onChangeListName(e){
+//     var newListName = e.target.value;
+//     setListName(newListName);
+    
 
 
-
-}
+// }
 
 
 function AddListName(props){
     return (
         <Form className="mx-2" >
             <InputGroup.Append className="my-1">
-            <Form.Control type="text" placeholder="new list name..." value={newListName} inline/>
+            <Form.Control type="text" placeholder="new list name..."  inline/>
             <Button variant="success" size="sm" className="ml-1">add</Button>
             </InputGroup.Append>
         </Form>
